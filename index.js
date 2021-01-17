e311a76a79c77c73068dee949bff8dfc149c494f
amoradi <moradi.aaron@gmail.com>
2021-01-17 02:40:24 -0500

    remove ssl lib

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const http = require('http')
 const fs = require('fs')
-const sslRedirect = require("heroku-ssl-redirect").default;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'content-type': 'text/html' })
   fs.createReadStream('index.html').pipe(res)
 })
-
-server.use(sslRedirect());
 
 server.listen(process.env.PORT || 3000);
